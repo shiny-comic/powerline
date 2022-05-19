cff9c53218c802a00cf35fe98b81b1f89953d007
PHP Wellnitz <philipwellnitz@gmx.de>
2022-05-19 16:21:06 +0900

    fix version generation

--- powerline/version.py
+++ powerline/version.py
@@ -2,15 +2,14 @@ import subprocess
 from traceback import print_exc
 
 try:
-    __version__ = subprocess.check_output(['git', 'describe', '--abbrev=0', '--tags']).strip().decode()
+    __version__ = subprocess.check_output(['git', 'describe', '--abbrev=0', '--tags'], stderr=subprocess.DEVNULL).strip().decode()
 except Exception:
-    print_exc()
-    __version__ = '1.9.4'
+    __version__ = '1.9.3'
 
 
 def get_version():
     try:
-        return __version__ + '.' + subprocess.check_output(['git', 'rev-list', '--count', __version__ + '..HEAD']).strip().decode()
+        return __version__ + '.' + subprocess.check_output(['git', 'rev-list', '--count', __version__ + '..HEAD'], stderr=subprocess.DEVNULL).strip().decode()
     except Exception:
         print_exc()
         return __version__
